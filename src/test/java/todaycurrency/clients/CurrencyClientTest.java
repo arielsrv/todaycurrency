d1da491d7e5d2a4167fc3a869e8aa19f659d1a79
arielsrv <arielsrv@users.noreply.github.com>
2022-02-07 11:35:34 +0100

    fix

--- src/test/java/todaycurrency/clients/CurrencyClientTest.java
+++ src/test/java/todaycurrency/clients/CurrencyClientTest.java
@@ -16,36 +16,37 @@ import static org.mockito.Mockito.when;
 
 public class CurrencyClientTest {
 
-	private RestClient restClient;
-	private CurrencyClient currencyClient;
+    private RestClient restClient;
+    private CurrencyClient currencyClient;
+    private Response response;
 
-	@BeforeEach
-	public void setUp() {
-		this.restClient = mock(RestClient.class);
-		this.currencyClient = new CurrencyClient(this.restClient);
-	}
+    @BeforeEach
+    public void setUp() {
+        this.restClient = mock(RestClient.class);
+        this.currencyClient = new CurrencyClient(this.restClient);
+        this.response = mock(Response.class);
+    }
 
-	@Test
-	public void get_currency() {
-		Response<Object> response = mock(Response.class);
-		when(response.getData()).thenReturn(getResponse());
-		when(this.restClient.get(any(), any())).thenReturn(Observable.just(response));
+    @Test
+    public void get_currency() {
+        when(response.getData()).thenReturn(getResponse());
+        when(this.restClient.get(any(), any())).thenReturn(Observable.just(response));
 
-		CurrencyResponse currencyResponse = this.currencyClient.getLatest().blockingFirst();
+        CurrencyResponse currencyResponse = this.currencyClient.getLatest().blockingFirst();
 
-		assertNotNull(currencyResponse);
-		assertNotNull(currencyResponse.oficial);
-		assertEquals(100.0, currencyResponse.oficial.value_buy);
-		assertEquals(100.0, currencyResponse.oficial.value_sell);
-	}
+        assertNotNull(currencyResponse);
+        assertNotNull(currencyResponse.oficial);
+        assertEquals(100.0, currencyResponse.oficial.value_buy);
+        assertEquals(100.0, currencyResponse.oficial.value_sell);
+    }
 
-	private CurrencyResponse getResponse() {
-		CurrencyResponse currencyResponse = new CurrencyResponse();
+    private CurrencyResponse getResponse() {
+        CurrencyResponse currencyResponse = new CurrencyResponse();
 
-		currencyResponse.oficial = new CurrencyResponse.Oficial();
-		currencyResponse.oficial.value_buy = 100.0;
-		currencyResponse.oficial.value_sell = 100.0;
+        currencyResponse.oficial = new CurrencyResponse.Oficial();
+        currencyResponse.oficial.value_buy = 100.0;
+        currencyResponse.oficial.value_sell = 100.0;
 
-		return currencyResponse;
-	}
+        return currencyResponse;
+    }
 }
